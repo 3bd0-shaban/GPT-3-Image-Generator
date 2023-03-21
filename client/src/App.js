447256a8094850539a69fd43f1344eb6e23855a3
AbdElra7man9 <abdelrahman201565@gmail.com>
2023-03-21 21:01:28 +0200

    fix deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Routes/Home';
 function App() {
   return (
+    
     <Routes>
       <Route index path='/' element={<Home />} />
     </Routes>
   );
 }
 
-export default App;
+export default App; 
