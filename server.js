0ccfab4c76f6997a942a74c1fe7bf1753ad5d741
AbdElra7man9 <abdelrahman201565@gmail.com>
2023-03-21 23:23:44 +0200

    no commit

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 app.listen(port, () => {
     console.log(`Run successfully at port ${port}`)
 });
-app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
